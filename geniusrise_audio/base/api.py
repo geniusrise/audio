4ec483abbe8a5d8294da9950a3def912fc913afc
ixaxaar <root@ixaxaar.in>
2024-01-16 17:21:27 +0530

    fix cors domain bug and add pre commit

--- geniusrise_audio/base/api.py
+++ geniusrise_audio/base/api.py
@@ -185,7 +185,7 @@ class AudioAPI(AudioBulk):
         )
 
         def CORS():
-            cherrypy.response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+            cherrypy.response.headers["Access-Control-Allow-Origin"] = cors_domain
             cherrypy.response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
             cherrypy.response.headers["Access-Control-Allow-Headers"] = "Content-Type"
             cherrypy.response.headers["Access-Control-Allow-Credentials"] = "true"
