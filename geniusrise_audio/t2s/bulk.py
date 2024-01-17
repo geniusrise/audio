4788cefd3181acf43903178c062003374b40188a
ixaxaar <root@ixaxaar.in>
2024-01-18 00:03:23 +0530

    add docs

--- geniusrise_audio/t2s/bulk.py
+++ geniusrise_audio/t2s/bulk.py
@@ -31,7 +31,7 @@ from torch import Tensor
 
 
 class TextToSpeechBulk(AudioBulk):
-    """
+    r"""
     TextToSpeechBulk is designed for bulk processing of text-to-speech tasks. It utilizes a range of models from Hugging Face,
     converting text inputs to speech audio outputs.
 
@@ -42,6 +42,49 @@ class TextToSpeechBulk(AudioBulk):
     Methods:
         synthesize_speech(texts: List[str], **kwargs: Any) -> None:
             Synthesizes speech from a batch of text inputs.
+
+    Example CLI Usage:
+    ```bash
+    genius TextToSpeechBulk rise \
+        batch \
+            --input_s3_bucket geniusrise-test \
+            --input_s3_folder input/summz \
+        batch \
+            --output_s3_bucket geniusrise-test \
+            --output_s3_folder output/summz \
+        postgres \
+            --postgres_host 127.0.0.1 \
+            --postgres_port 5432 \
+            --postgres_user postgres \
+            --postgres_password postgres \
+            --postgres_database geniusrise\
+            --postgres_table state \
+        --id facebook/bart-large-cnn-lol \
+        summarize \
+            --args \
+                model_name="facebook/bart-large-cnn" \
+                model_class="AutoModelForSeq2SeqLM" \
+                tokenizer_class="AutoTokenizer" \
+                use_cuda=True \
+                precision="float" \
+                quantization=0 \
+                device_map="cuda:0" \
+                max_memory=None \
+                torchscript=False \
+                generation_bos_token_id=0 \
+                generation_decoder_start_token_id=2 \
+                generation_early_stopping=true \
+                generation_eos_token_id=2 \
+                generation_forced_bos_token_id=0 \
+                generation_forced_eos_token_id=2 \
+                generation_length_penalty=2.0 \
+                generation_max_length=142 \
+                generation_min_length=56 \
+                generation_no_repeat_ngram_size=3 \
+                generation_num_beams=4 \
+                generation_pad_token_id=1 \
+                generation_do_sample=false
+    ```
     """
 
     model: AutoModelForCTC
